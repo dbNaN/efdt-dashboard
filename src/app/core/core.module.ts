ca83336cd099df3bbd57c7602e694e01f2be14b9
dbNaN <damiano.biasioli93@gmail.com>
2021-04-22 15:22:57 +0200

    Fixed sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { StoreService } from './services/store.service';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { PaginationService } from './services/pagination.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [HeaderComponent, SidebarComponent],
   exports: [HeaderComponent, SidebarComponent],
   providers: [ApiService, StoreService, PaginationService],
