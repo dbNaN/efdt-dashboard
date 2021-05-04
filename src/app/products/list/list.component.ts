b43706cfb45e50a17ad33124059282be0a0e40e5
dbNaN <damiano.biasioli93@gmail.com>
2021-05-04 11:45:55 +0200

    Fixed chart background color

--- src/app/products/list/list.component.ts
+++ src/app/products/list/list.component.ts
@@ -15,84 +15,11 @@ import { StoreService } from 'src/app/core/services/store.service';
 export class ListComponent implements OnInit {
   public pagedItems: any[];
   public setPage: boolean = false;
-  public products: any[] = [
-    {
-      id: '3dkjcHtVCku9xML1gS6G',
-      data: {
-        description: 'fdsfds',
-        price: 250,
-        title: 'lplp',
-        employee: 'fdssd',
-        category: 'fds',
-      },
-    },
-    {
-      id: '430sQSsxwFAIviXFBlCs',
-      data: {
-        title: 'a',
-        employee: 'fdssd',
-        price: 0,
-        category: 'z',
-        description: 'w',
-      },
-    },
-    {
-      id: '7LCrVrl98vJ1ltNKnO77',
-      data: {
-        category: 'Pasticceria',
-        title: 'Torta sake',
-        description: 'Torta al sake caldo',
-        employee: 'Giovanni Sempronio Agamennone',
-        price: 100000000000000000,
-      },
-    },
-    {
-      id: 'ZDXfjxwDRe1aOnty6UUv',
-      data: {
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae mi tellus. Sed viverra lacus dapibus metus tincidunt malesuada. Morbi imperdiet iaculis diam. Vivamus malesuada nisi sed lacus laoreet, id efficitur enim vestibulum. Donec ac risus eget nibh commodo pellentesque at id risus. Ut nec erat est. Nam nec bibendum enim. Phasellus ac efficitur sapien. Etiam at ligula et orci ultrices sollicitudin in vitae est.',
-        title: 'Long description',
-        price: '55',
-        category: 'testing',
-        id: 'id1',
-        employee: 'Giovanni',
-      },
-    },
-    {
-      id: 'aPf4UL223BKdhuYkrxXd',
-      data: {
-        category: 'Panini dolci',
-        description: 'EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEh',
-        price: 10,
-        title: 'panino al sambuco',
-        employee: 'Giovanni',
-      },
-    },
-    {
-      id: 'cgkMKOKLFuATjtiVpboo',
-      data: {
-        review: '',
-        description: 'pollo con patate',
-        title: 'pollo con patate',
-        employee: 'Aldo',
-        price: '15',
-        category: 'secondo',
-      },
-    },
-    {
-      id: 'osKO5guWuoABgKjIWK3M',
-      data: {
-        title: 'torta alla vaniglia',
-        employee: 'Salgemma Lorenza',
-        price: 240,
-        category: 'torte',
-        description: 'Torta di buona fattura',
-      },
-    },
-  ];
+  public products: any[] = [];
   public isGridLayout: boolean;
   public polarAreaChartLabels: Label[] = [];
   public polarAreaChartData: SingleDataSet = [];
+  public isChartVisible: boolean = false;
 
   public polarAreaLegend: boolean = true;
   public polarAreaChartType = 'polarArea';
@@ -139,18 +66,18 @@ export class ListComponent implements OnInit {
   getStats() {
     this.storeService
       .getCategoriesStats()
-      .subscribe(async (response: CategoryStats[]) => {
+      .subscribe((response: CategoryStats[]) => {
         let labels = [];
         let data = [];
         let color = [];
-        await response.forEach((e) => {
+        response.forEach((e) => {
           labels.push(e.category);
           data.push(e.numberOfProducts);
-          color.push(this.randomRGB());
         });
         this.polarAreaChartLabels = labels;
         this.polarAreaChartData = data;
         this.polarAreaColors = color;
+        this.isChartVisible = true;
       });
   }
 
@@ -162,13 +89,6 @@ export class ListComponent implements OnInit {
     this.storeService.deleteProduct(id).subscribe(() => {
       this.getProducts();
     });
-  }
-
-  randomRGB() {
-    const r = Math.random() * 255;
-    const g = Math.random() * 255;
-    const b = Math.random() * 255;
-    return `rgba(${r},${g},${b},0.28)`;
   }
 
   getItems(event) {
